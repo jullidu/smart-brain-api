e12ed97fa453e13fd0eabd4d713b37952f8008a2
Юлия Михайлова <jullidu@gmail.com>
2019-11-29 14:01:26 +0800

    added connection signin form in frontend

--- server.js
+++ server.js
@@ -1,96 +1,102 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
 
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
 
 const dataBase = {
   users: [{
-      id: '123',
-      name: 'John',
-      email: 'john@gmail.com',
-      password: 'cookies',
+      id: "123",
+      name: "John",
+      email: "john@gmail.com",
+      password: "cookies",
       entries: 0,
       joined: new Date()
     },
     {
-      id: '124',
-      name: 'Sally',
-      email: 'sally@gmail.com',
-      password: 'bananas',
+      id: "124",
+      name: "Sally",
+      email: "sally@gmail.com",
+      password: "bananas",
       entries: 0,
       joined: new Date()
     }
   ]
-}
+};
 
-app.get('/', (req, res) => {
-  res.send(dataBase.users)
-})
+app.get("/", (req, res) => {
+  res.send(dataBase.users);
+});
 
-app.post('/signin', (req, res) => {
-  if (req.body.email === dataBase.users[0].email &&
-    req.body.password === dataBase.users[0].password) {
-    res.json('success')
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === dataBase.users[0].email &&
+    req.body.password === dataBase.users[0].password
+  ) {
+    res.json("success");
   } else {
-    req.status(400).json('error logging')
+    res.status(400).json("error logging");
   }
-})
+});
 
-app.post('/register', (req, res) => {
+app.post("/register", (req, res) => {
   const {
     name,
     email,
     password
-  } = req.body
+  } = req.body;
   dataBase.users.push({
-    id: '125',
+    id: "125",
     name: name,
     email: email,
     password: password,
     entries: 0,
     joined: new Date()
-  })
-  res.json(dataBase.users[dataBase.users.length - 1])
-})
+  });
+  res.json(dataBase.users[dataBase.users.length - 1]);
+});
 
-app.get('/profile/:id', (req, res) => {
+app.get("/profile/:id", (req, res) => {
   const {
     id
-  } = req.params
+  } = req.params;
 
-  let found = false
+  let found = false;
 
   dataBase.users.forEach(user => {
     if (user.id === id) {
-      found = true
-      res.json(user)
+      found = true;
+      res.json(user);
     }
-  })
+  });
   if (!found) {
-    res.status(404).json('not found ')
+    res.status(404).json("not found ");
   }
-})
+});
 
-app.put('/image', (req, res) => {
+app.put("/image", (req, res) => {
   const {
     id
-  } = req.body
+  } = req.body;
 
-  let found = false
+  let found = false;
 
   dataBase.users.forEach(user => {
     if (user.id === id) {
-      found = true
-      user.entries++
-      return res.json(user.entries)
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
     }
-  })
+  });
   if (!found) {
-    res.status(404).json('not found')
+    res.status(404).json("not found");
   }
-})
+});
 
 app.listen(3000, () => {
-  console.log('app is running')
-})
+  console.log("app is running");
+});
