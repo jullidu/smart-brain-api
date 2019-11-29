0aba8bba855b81ed226fe0f1546c0e6035362eb8
Юлия Михайлова <jullidu@gmail.com>
2019-11-29 10:08:22 +0800

    start create server

---
+++ server.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express()
+app.use(bodyParser.json())
+
+const dataBase = {
+  users: [{
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: 'sally@gmail.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+}
+
+app.get('/', (req, res) => {
+  res.send(dataBase.users)
+})
+
+app.post('/signin', (req, res) => {
+  if (req.body.email === dataBase.users[0].email &&
+    req.body.password === dataBase.users[0].password) {
+    res.json('success')
+  } else {
+    req.status(400).json('error logging')
+  }
+})
+
+app.post('/register', (req, res) => {
+  const {
+    name,
+    email,
+    password
+  } = req.body
+  dataBase.users.push({
+    id: '125',
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  })
+  res.json(dataBase.users[dataBase.users.length - 1])
+})
+
+app.get('/profile/:id', (req, res) => {
+  const {
+    id
+  } = req.params
+
+  let found = false
+
+  dataBase.users.forEach(user => {
+    if (user.id === id) {
+      found = true
+      res.json(user)
+    }
+  })
+  if (!found) {
+    res.status(404).json('not found ')
+  }
+})
+
+app.put('/image', (req, res) => {
+  const {
+    id
+  } = req.body
+
+  let found = false
+
+  dataBase.users.forEach(user => {
+    if (user.id === id) {
+      found = true
+      user.entries++
+      return res.json(user.entries)
+    }
+  })
+  if (!found) {
+    res.status(404).json('not found')
+  }
+})
+
+app.listen(3000, () => {
+  console.log('app is running')
+})
