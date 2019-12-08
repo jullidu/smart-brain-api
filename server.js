ec062c4e6e72edc62efde51ae52e5605692cc40e
Юлия Михайлова <jullidu@gmail.com>
2019-12-08 12:34:18 +0800

    changing response

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
 
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image')
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
 
 const db = knex({
   client: "pg",
@@ -25,27 +25,27 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send(dataBase.users);
+  res.send("it is working!");
 });
 
 app.post("/signin", (req, res) => {
-  signin.handleSignin(req, res, db, bcrypt)
+  signin.handleSignin(req, res, db, bcrypt);
 });
 
 app.post("/register", (req, res) => {
-  register.handleRegister(req, res, db, bcrypt)
+  register.handleRegister(req, res, db, bcrypt);
 });
 
 app.get("/profile/:id", (req, res) => {
-  profile.handleProfileGet(req, res, db)
+  profile.handleProfileGet(req, res, db);
 });
 
 app.put("/image", (req, res) => {
-  image.handleImage(req, res, db)
+  image.handleImage(req, res, db);
 });
 
 app.post("/imageurl", (req, res) => {
-  image.handleApiCall(req, res)
+  image.handleApiCall(req, res);
 });
 
 app.listen(process.env.PORT || 3000, () => {
